77c4eca49890e8d917ccbcfdfc68b5261bb40880
Danilo de Maria <daniloctg@msn.com>
2021-09-29 14:44:27 -0300

    feat: localizacao por coordenada

--- routes/localizacao.js
+++ routes/localizacao.js
@@ -3,14 +3,16 @@ const {
   criarLocalizacao,
   buscarTodasLocalizacoesDispositivo,
 } = require('../controllers/localizacao');
+const { buscarUmDispositivo } = require('../controllers/dispositivo');
 const router = express.Router();
 
 // rota GET para buscar todas as localizações cadastrados
-router.get('/:id_dispositivo', async (req, res, next) => {
-  const { id_dispositivo } = req.params;
+router.get('/:uuid', async (req, res, next) => {
+  const { uuid } = req.params;
   try {
+    const dispositivo = await buscarUmDispositivo({ uuid });
     const localizacoes = await buscarTodasLocalizacoesDispositivo({
-      id_dispositivo,
+      id_dispositivo: dispositivo.id,
     });
     return res.status(200).send(localizacoes);
   } catch (error) {
@@ -20,16 +22,31 @@ router.get('/:id_dispositivo', async (req, res, next) => {
 
 // rota POST para criar uma nova localizacao no banco de dados
 router.post('/', async (req, res, next) => {
-  const { id_dispositivo, latitude, longitude } = req.body;
+  const { uuid, latitude, longitude } = req.body;
   try {
+    const dispositivo = await buscarUmDispositivo({ uuid });
     const localizacaoCriada = await criarLocalizacao(
-      id_dispositivo,
+      dispositivo.id,
       latitude,
       longitude
     );
     return res
       .status(200)
       .send({ message: 'Localização criado com sucesso!', localizacaoCriada });
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+});
+
+// rota GET para buscar através da localização informada, se existe um objeto próximo
+router.get('/obstaculo/:latitude/:longitude', async (req, res, next) => {
+  const { latitude, longitude } = req.params;
+  try {
+    const obstaculo = await buscarTodasLocalizacoesDispositivo({
+      latitude,
+      longitude,
+    });
+    return res.status(200).send(obstaculo.length > 0);
   } catch (error) {
     return res.status(500).send(error);
   }
